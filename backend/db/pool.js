50022a2bfe97823b70a476365218c8ff2170a5a4
KevinDeveloperJr <keviinramiirez2504@gmail.com>
2025-10-28 08:53:34 -0600

    Update pool.js

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: process.env.PGHOST || '172.235.145.142',
   database: process.env.PGDATABASE || 'seminarioclinic',
   user: process.env.PGUSER || 'seminario',
-  password: process.env.PGPASSWORD || 'seminario2025',
+  password: process.env.PGPASSWORD || 'seminario321_0',
   port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
   max: process.env.PGPOOL_MAX ? Number(process.env.PGPOOL_MAX) : 10,
   idleTimeoutMillis: 30000,
