af300f1d4d17895e0f1bc2b369238a3450f141e6
KevinDeveloperJr <keviinramiirez2504@gmail.com>
2025-10-27 21:50:49 -0600

    Update pool.js

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 // Único pool compartido en toda la app
 // Config utilizable con variables de entorno o valores por defecto de desarrollo
 const pool = new Pool({
-  host: process.env.PGHOST || '127.0.0.1',
+  host: process.env.PGHOST || '172.235.135.155',
   database: process.env.PGDATABASE || 'seminarioclinic',
   user: process.env.PGUSER || 'seminario',
-  password: process.env.PGPASSWORD || 'seminario321_0',
+  password: process.env.PGPASSWORD || 'seminario2025',
   port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
   max: process.env.PGPOOL_MAX ? Number(process.env.PGPOOL_MAX) : 10,
   idleTimeoutMillis: 30000,
